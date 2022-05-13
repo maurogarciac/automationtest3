4426654ffc48f6619163514167679b0a5312f163
Mauro Garcia Coto <Mauro.Garcia@QUALITEST.LOCAL>
2022-05-13 11:49:12 -0300

    Added a script for excel

---
+++ Scripts/excel.py
@@ -0,0 +1,11 @@
+from appium import webdriver
+from appium.webdriver.common.mobileby import MobileBy
+
+desired_caps = {}
+desired_caps["app"] = "Excel"
+driver = webdriver.Remote(
+    'http://127.0.0.1:4723',
+    desired_caps)
+
+screenshotBase64 = driver.get_screenshot_as_base64()
+screenshotBase64
