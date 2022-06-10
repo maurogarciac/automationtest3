5296d45f10bcfe9f085d7b04c0c69cda3b9d6bf2
Mauro Garcia Coto <maurogarciacoto@gmail.com>
2022-06-10 19:46:02 -0300

    Image recognition is working now, updated README

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # automationtest3
 
+1. Install Android Studio or similar
+1. Install Appium Server
+
+
+
+### Installation process for OpenCV for NodeJS
+1. Install NodeJS
+   * In case a permissions error arises for the following commands, exclude npm folders in Windows Defender.
+1. Install NPM version 6.x.x
+   ```npm install --global npm@6```
+1. Install OpenCV for NodeJS
+   ```npm install --global opencv4nodejs```
 --- 
 
 automation using appium drivers
--- Scripts/calculator2.py
+++ Scripts/calculator2.py
@@ -1,4 +1,5 @@
-
+import os
+import base64
 from appium import webdriver
 from appium.webdriver.common.mobileby import MobileBy
 
@@ -8,11 +9,19 @@ desired_caps = {
     "platformName" : "Android",
     "platformVersion" : "11.0",
     "deviceName" : "pixel_2",
-    "app" : "F:\COSAS IMPORTANTES\SCRIPSTSTSTSTS\Calculator_v8.1 (403424005)_apkpure.com.apk"
+    "app" : "F:\COSAS IMPORTANTES\SCRIPSTSTSTSTS\Calculator_v8.1 (403424005)_apkpure.com.apk",
+    "fixImageTemplateScale" : False,
+    "fixImageTemplateSize" : False,
+    "imageMatchThreshold" : 1,
+    "fixImageFindScreenshotDims" : False,
+    "imageMatchMethod" : "TM_SQDIFF_NORMED",
+    "getMatchedImageResult" : True
 }
 driver = webdriver.Remote(
     'http://127.0.0.1:4723/wd/hub',
     desired_caps)
+
+# driver.update_settings({"fixImageTemplateScale" : False})
 
 
 try:
@@ -22,14 +31,16 @@ try:
     equals_button = driver.find_element(MobileBy.ID, "eq")
     display = driver.find_element(MobileBy.ID, "formula")
     pre_display = driver.find_element(MobileBy.ID, "result_preview")
-    minus_android = driver.find_element(MobileBy.IMAGE, "minus_android.png")
-
+    #minus_android = driver.find_element_by_image("minus_android.png")
+    with open("minus_android.png", 'rb') as i_file:
+            b64_data = base64.b64encode(i_file.read()).decode('UTF-8')
+    minus_android = driver.find_element(MobileBy.IMAGE, b64_data)
 
     seven_button.click()
     add_button.click()
     nine_button.click()
     minus_android.click()
-    
+    nine_button.click()
     print(pre_display.text)
     equals_button.click()
     #print(display.text)
